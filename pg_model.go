06623835579d459b8f43914db784f1db5faee208
Anders Janmyr <anders@janmyr.com>
2017-05-30 14:21:18 -0700

    Added pg_model

---
+++ pg_model.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type PgJobRepo struct {
+	db *gorm.DB
+}
+
+func NewPgJobRepo(db *gorm.DB) *PgJobRepo {
+	return &PgJobRepo{
+		db: db,
+	}
+}
+
+func (r *PgJobRepo) Find() ([]*Job, error) {
+	var jobs []*Job
+	if err := r.db.Find(&jobs).Error; err != nil {
+		return nil, err
+	}
+	return jobs, nil
+}
+
+func (r *PgJobRepo) FindOne(slug string) (*Job, error) {
+	job := Job{}
+	if err := r.db.Where(&Job{Slug: slug}).First(&job).Error; err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (r *PgJobRepo) Add(job *Job) (*Job, error) {
+	if err := r.db.Create(job).Error; err != nil {
+		return nil, err
+	}
+	return job, nil
+}
+
+func (r *PgJobRepo) Update(job *Job) (*Job, error) {
+	newJob := *job
+	if err := r.db.Save(&newJob).Error; err != nil {
+		return nil, err
+	}
+	return &newJob, nil
+}
+
+func (r *PgJobRepo) UpAdd(job *Job) (*Job, error) {
+	return nil, nil
+}
+
+func (r *PgJobRepo) Delete(slug string) (*Job, error) {
+	job, err := r.FindOne(slug)
+	if err != nil {
+		return nil, err
+	}
+	if err := r.db.Delete(job).Error; err != nil {
+		return nil, err
+	}
+	return job, nil
+}
---
+++ pg_model_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+var db *gorm.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	db, err = gorm.Open("postgres", "host=localhost user=jobs dbname=jobs sslmode=disable password=jobs")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Job{})
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func TestPgJobsAddFind(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+	jobRepo := NewPgJobRepo(tx)
+	job := NewJob("Dingo")
+	job, err := jobRepo.Add(job)
+	assert.Nil(t, err)
+	jobs, err := jobRepo.Find()
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(jobs))
+}
+
+func TestPgJobsUpdate(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+	jobRepo := NewPgJobRepo(tx)
+	job := NewJob("Dingo")
+	job, err := jobRepo.Add(job)
+	job.Name = "Sloth"
+	updatedJob, err := jobRepo.Update(job)
+	assert.Nil(t, err)
+	assert.True(t, updatedJob.UpdatedAt.After(job.UpdatedAt), "Expected '%v' to be after '%v'", updatedJob.UpdatedAt, job.UpdatedAt)
+}
+
+func TestPgJobsFindOne(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+	jobRepo := NewPgJobRepo(tx)
+	jobRepo.Add(NewJob("Dingo"))
+	jobRepo.Add(NewJob("Sloth"))
+	job, err := jobRepo.FindOne("sloth")
+	assert.Nil(t, err)
+	assert.Equal(t, "Sloth", job.Name)
+}
+
+func TestPgJobsDelete(t *testing.T) {
+	tx := db.Begin()
+	defer tx.Rollback()
+	jobRepo := NewPgJobRepo(tx)
+	jobRepo.Add(NewJob("Dingo"))
+	jobRepo.Add(NewJob("Sloth"))
+	job, err := jobRepo.Delete("sloth")
+	assert.Nil(t, err)
+	assert.Equal(t, "Sloth", job.Name)
+	jobs, err := jobRepo.Find()
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(jobs))
+}
